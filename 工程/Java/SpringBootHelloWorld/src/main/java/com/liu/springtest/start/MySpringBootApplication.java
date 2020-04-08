fb8f3b09f93759be061da92966afcb781414a791
liufeng.macpro <dev.liufeng@gmail.com>
2020-04-08 22:11:41 +0800

    study at home

--- 工程/Java/SpringBootHelloWorld/src/main/java/com/liu/springtest/start/MySpringBootApplication.java
+++ 工程/Java/SpringBootHelloWorld/src/main/java/com/liu/springtest/start/MySpringBootApplication.java
@@ -2,10 +2,12 @@ package com.liu.springtest.start;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class MySpringBootApplication {
     public static void main(String[] args) {
+        new SpringApplicationBuilder().run()
         SpringApplication.run(MySpringBootApplication.class, args);
     }
 }
