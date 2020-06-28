a8e6b2e6befb2aac3ccd9e5c9b459944a3b1aa3d
liufeng <dev.liufeng@gmail.com>
2020-06-29 07:59:43 +0800

    新内容

--- 工程/VUE/juner-web/src/App.vue
+++ 工程/VUE/juner-web/src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/background.jpg" width="100%" height="100%">
+    <div class="contain">
+<!--      <img src="./assets/background.jpg" class="image">-->
+<!--      <input placeholder="请输入手机号">-->
+<!--      <input placeholder="请输入密码">-->
+    </div>
+    <button class="button">登录/注册</button>
   </div>
 </template>
 
@@ -8,13 +13,28 @@
 
 export default {
   name: 'App',
-  components: {
-  }
+  components: {}
 }
 </script>
 
-<style>
-#app {
-  margin-inside: 0;
-}
+<style scoped>
+  #app{
+      /*background-color: lightgray;*/
+      width: 100%;
+      height: 100%;
+      position: absolute;
+  }
+  .image {
+    width: 100%;
+    height: 100%;
+  }
+  .contain{
+      align-content: center;
+  }
+    .button{
+        width: 200px;
+        height: 50px;
+        background-color: #2c3e50;
+        color: white;
+    }
 </style>
