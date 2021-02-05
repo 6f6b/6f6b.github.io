d742e968437ab5f112e212bf08df073cf1f0f7b5
liufeng <liufeng@postop.cn>
2021-02-05 18:54:04 +0800

    【new】

---
+++ 工程/Java/mcservice/src/main/java/com/example/mcservice/McserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.mcservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McserviceApplication.class, args);
+    }
+
+}
---
+++ 工程/Java/mcservice/src/test/java/com/example/mcservice/McserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mcservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
