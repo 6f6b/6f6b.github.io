e50366dfb112d1462ea00d9ed7463b11adc5384e
liufeng <liufeng@postop.cn>
2021-09-11 13:17:28 +0800

    新内容

--- 工程/Java/eurekaclient02/src/main/java/com/example/eurekaclient02/Eurekaclient02Application.java
+++ 工程/Java/eurekaclient02/src/main/java/com/example/eurekaclient02/Eurekaclient02Application.java
@@ -3,6 +3,8 @@ package com.example.eurekaclient02;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,4 +14,8 @@ public class Eurekaclient02Application {
         SpringApplication.run(Eurekaclient02Application.class, args);
     }
 
+    @Bean
+    RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 }
--- 工程/Java/eurekaclient02/src/main/java/com/example/eurekaclient02/controller/HomeController.java
+++ 工程/Java/eurekaclient02/src/main/java/com/example/eurekaclient02/controller/HomeController.java
@@ -6,14 +6,39 @@ import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @RestController
 public class HomeController {
     @Autowired
     private DiscoveryClient discoveryClient;
+    @Autowired
+    private RestTemplate restTemplate;
 
     @RequestMapping("/hello")
-    public String sayHello(){
-        return "hello im liufeng , from client 02";
+    public String sayHello(HttpServletRequest request){
+        System.out.println("请求了hello");
+        ServiceInstance instance = getInstanceWith(request,"eureka-client-3");
+        String result = restTemplate.getForObject(uriWith(instance,"/hello"), String.class);
+        return "hello im liufeng , from client 02\n"+result;
+    }
+
+    private String uriWith(ServiceInstance instance,String path){
+        String uri = String.format("http://%s:%d%s",instance.getHost(),instance.getPort(),path);
+        return uri;
+    }
+
+    private ServiceInstance getInstanceWith(HttpServletRequest request,String serviceId){
+        //Header中是否有DEBUG标识
+        List<ServiceInstance> instances = discoveryClient.getInstances(serviceId);
+        ServiceInstance instance = instances.stream().findAny().get();
+        if ("DEBUG".equals(request.getHeader("RUNNING-MODEL"))){
+            ServiceInstance temIns = instances.stream().filter((ins)->{return ins.getHost().equals(request.getRemoteAddr());}).findFirst().get();
+            instance = (temIns == null) ? instance : temIns;
+        }
+        return instance;
     }
 }
